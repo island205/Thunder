e5f735a035536dd0974382f43ccb1619581eabe4
Kane Xiao <dmccer@gmail.com>
2013-01-19 05:26:26 +0800

    bump

--- server/thunder/public/javascripts/bump.js
+++ server/thunder/public/javascripts/bump.js
@@ -1,82 +1,92 @@
-var ifBumpThreshold = 2.5;
-var speedArray = new Array();
-var timeSpan = 50;
-var watchID = null;
-var geoWatchID = null;
-var onBumpToDo;
-var latitude = 0;
-var longitude = 0;
-var altitude = 0;
+(function (WIN) {
+    var B = WIN.B || {};
 
-function startBump(onBump) {
-    onBumpToDo = onBump;
+    var loginTime = Date.now();
 
-    var options = {
-        frequency: timeSpan
-    };
+    var ifBumpThreshold = 2.5;
+    var speedArray = new Array();
+    var timeSpan = 50;
+    var watchID = null;
+    var geoWatchID = null;
+    var onBumpToDo;
+    var latitude = 0;
+    var longitude = 0;
+    var altitude = 0;
 
-    watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
-    geoWatchID = navigator.geolocation.watchPosition(onGeoSuccess, onError, {
-        timeout: 1000
-    });
-}
+    function startBump(onBump) {
+        onBumpToDo = onBump;
 
-function stopBump() {
-    if(watchID) {
-        navigator.accelerometer.clearWatch(watchID);
-        watchID = null;
+        var options = {
+            frequency: timeSpan
+        };
+
+        watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+        geoWatchID = navigator.geolocation.watchPosition(onGeoSuccess, onError, {
+            timeout: 1000
+        });
     }
-}
 
-function onGeoSuccess(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    altitude = position.coords.altitude;
-}
-
-function onSuccess(acceleration) {
-    addSpeed(acceleration.x, acceleration.y, acceleration.z);
-    if(isBump()) {
-        onBumpToDo(latitude,longitude,new Date());
+    function stopBump() {
+        if(watchID) {
+            navigator.accelerometer.clearWatch(watchID);
+            watchID = null;
+        }
     }
-}
 
-function onError() {}
-
-function mean(a) {
-    var sum = eval(a.join(" + "));
-    return sum / a.length;
-}
-
-function stdDev() {
-    var a = speedArray;
-    var m = mean(a);
-    var sum = 0;
-    var l = a.length;
-    for(var i = 0; i < l; i++) {
-        var dev = a[i] - m;
-        sum += (dev * dev);
+    function onGeoSuccess(position) {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        altitude = position.coords.altitude;
     }
-    return Math.sqrt(sum / (l - 1));
-}
 
-function getSpeed(x, y, z) {
-    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
-}
-
-function addSpeed(x, y, z) {
-    var speed = getSpeed(x, y, z);
-    speedArray.push(speed);
-    if(speedArray.length > 10) {
-        speedArray.shift();
+    function onSuccess(acceleration) {
+        addSpeed(acceleration.x, acceleration.y, acceleration.z);
+        if(isBump()) {
+            onBumpToDo(latitude, longitude, '' + loginTime + Date.now());
+        }
     }
-}
 
-function isBump() {
-    var s = stdDev();
-    var bump = s > ifBumpThreshold;
-    if(bump) {
-        speedArray = new Array();
+    function onError() {}
+
+    function mean(a) {
+        var sum = eval(a.join(" + "));
+        return sum / a.length;
     }
-    return bump;
-}
+
+    function stdDev() {
+        var a = speedArray;
+        var m = mean(a);
+        var sum = 0;
+        var l = a.length;
+        for(var i = 0; i < l; i++) {
+            var dev = a[i] - m;
+            sum += (dev * dev);
+        }
+        return Math.sqrt(sum / (l - 1));
+    }
+
+    function getSpeed(x, y, z) {
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+    }
+
+    function addSpeed(x, y, z) {
+        var speed = getSpeed(x, y, z);
+        speedArray.push(speed);
+        if(speedArray.length > 10) {
+            speedArray.shift();
+        }
+    }
+
+    function isBump() {
+        var s = stdDev();
+        var bump = s > ifBumpThreshold;
+        if(bump) {
+            speedArray = new Array();
+        }
+        return bump;
+    }
+
+    B.bump = startBump;
+})(window);
+
+
--- server/thunder/public/javascripts/customer.js
+++ server/thunder/public/javascripts/customer.js
@@ -1,88 +1,115 @@
-$(function () {
-    var
-    DOC = document,
-    fNOP = function () {},
-    Overlay,
-    Tickets
+(function (WIN){
+    var B = WIN.B || {};
 
-    // socket.io
-    var
-    socket
-
-    socket = io.connect(window.location.origin)
-
-    socket.on('connected', function (data) {
-        console.log(data)
-    })
-
-    // socket.on('bump', function (data) {
-    //     console.log(data)
-    // })
-    
-    // socket.emit('bump', {id: 54365 })
-
-    Overlay = (function () {
+    $(function () {
         var
-        tipWrapper = $('.tip-wrapper'),
-        tipInfo = $('.tip-info'),
-        overlay = $('.overlay')
+        DOC = document,
+        fNOP = function () {},
+        overlay,
+        tickets
 
-        tipWrapper.hide();
+        // socket.io
+        var
+        socket
 
-        return {
-            tpls: {
-                searching: $('#tplSearching').html(),
-                netWorkError: $('#tplNetWorkError').html(),
-                confirmInfo: $('#tplConfirmInfo').html(),
-                tip1: $('#tplTip1').html(),
-                tip2: $('#tplTip2').html(),
-                tip3: $('#tplTip3').html()
-            },
+        socket = io.connect(window.location.origin)
 
-            __show: function () {               
-                tipWrapper.slideDown()      
-            },
-            hide: function () {
-                tipWrapper.hide()
-                overlay.hide()    
-            },
-            show: function (tpl) {
-                tipInfo.html(this.tpls[tpl])
-                this.__show()
-            }
-        };
-    })();
-
-    new iScroll('content-wrapper', {
-        hScroll: false,
-        hScrollbar: false,
-        checkDOMChanges: false
-    });
-
-    Tickets = (function () {
-        var $tickets = $('.ticket-item')
-
-        $tickets.click(function () {
-            $(this).toggleClass('selected')
+        socket.on('connected', function (data) {
+            console.log(data)
         })
 
-
-    });
-    
-
-    function onBump(latitude, longitude, date) {
-        Overlay.show('tip1')
-        // TODO:
-        // 1. 获取选择的数据发送给服务器
+        // socket.on('bump', function (data) {
+        //     console.log(data)
+        // })
         
-    }
+        // socket.emit('bump', {id: 54365 })
 
-    function onDeviceReady() {
-        startBump(onBump);
-    }
+        overlay = (function () {
+            var
+            tipWrapper = $('.tip-wrapper'),
+            tipInfo = $('.tip-info'),
+            overlay = $('.overlay')
 
-    DOC.addEventListener("deviceready", onDeviceReady, false);
+            tipWrapper.hide();
 
+            return {
+                tpls: {
+                    searching: $('#tplSearching').html(),
+                    netWorkError: $('#tplNetWorkError').html(),
+                    confirmInfo: $('#tplConfirmInfo').html(),
+                    tip1: $('#tplTip1').html(),
+                    tip2: $('#tplTip2').html(),
+                    tip3: $('#tplTip3').html()
+                },
 
-})
+                __show: function () {               
+                    tipWrapper.slideDown()      
+                },
+                hide: function () {
+                    tipWrapper.hide()
+                    overlay.hide()    
+                },
+                show: function (tpl) {
+                    tipInfo.html(this.tpls[tpl])
+                    this.__show()
+                }
+            };
+        })();
+
+        new iScroll('content-wrapper', {
+            hScroll: false,
+            hScrollbar: false,
+            checkDOMChanges: false
+        });
+
+        tickets = (function () {
+            var $tickets = $('.ticket-item'),
+                serials = []
+
+            $tickets.click(function () {
+                var serial = $(this).attr('data-serial');
+
+                $(this).toggleClass('selected')
+
+                if ($(this).hasClass('selected')) {
+                    serials.push(serial)
+                } else {
+                    serials = _.without(serials, serial)
+                }
+            })
+
+            return {
+                get: function () {
+                    return serials;
+                }
+            }
+        });
+        
+
+        function onBump(latitude, longitude, id) {
+            var d = {
+                id: id,
+                lat: latitude,
+                lon: longitude,
+                type: 'customer',
+                serial: tickets.get()
+            }
+
+            overlay.show('tip1')
+
+            socket.emit('bump', d)
+
+            // TODO:
+            // 1. 获取选择的数据发送给服务器
+            
+        }
+
+        function onDeviceReady() {
+            B.bump(onBump);
+        }
+
+        DOC.addEventListener("deviceready", onDeviceReady, false);
+    })
+})(window);
+
 
