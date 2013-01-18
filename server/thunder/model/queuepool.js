f4f7794c97c10a99e2881bfdba0d44732b8f5fea
Pieux Xi <pieuxxi@gmail.com>
2013-01-19 02:01:46 +0800

    return serial operate status

--- server/thunder/model/queuepool.js
+++ server/thunder/model/queuepool.js
@@ -13,3 +13,14 @@ QueuePool.prototype.clear = function() {
     this.async(function() {
     });
 }
+
+QueuePool.prototype.serialOperateStatus = function (serial) {
+    var status = [];
+    for(var i=0; i<serial.length; i++){
+
+        status.push(serial[i] + ", " + function () {
+            return Math.random() < 0.5 ? "成功" : "失败";
+        }();
+    }
+    return status;
+}
