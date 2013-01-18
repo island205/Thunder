94dcbb5d6ffc72b4f71f5734aeb487b91b11a501
Dozer <mail@dozer.cc>
2013-01-18 18:03:52 +0800

    add thunder shop

--- android/customer/src/com/dianping/hackathon/thunder/customer/MainActivity.java
+++ android/customer/src/com/dianping/hackathon/thunder/customer/MainActivity.java
@@ -9,14 +9,6 @@ public class MainActivity extends DroidGap{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("http://192.168.13.152:3000/");
+        super.loadUrl("http://192.168.62.110:3000");
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
 }
