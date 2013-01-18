ee13ef77c8714ad02578fbf37394b6420e1a69fd
Dozer <mail@dozer.cc>
2013-01-19 00:31:07 +0800

    add queue model

--- server/thunder/model/customer.js
+++ server/thunder/model/customer.js
@@ -1,32 +1,9 @@
-module.export = Customer;
+module.export = QueuePool;
 
-
-function Customer(lat, lon, bt) {
-    this.latitude = lat;
-    this.longtitude = lon;
-    this.bumpTime = bt;
+function QueuePool() {
+    this.pool = new Array();
 }
 
-Customer.prototype.setLocation = function (lat, lon) {
-    this.latitude = lat;
-    this.longtitude = lon;
-}
-
-Customer.prototype.setBumpTime = function (bt) {
-    this.bumpTime = bt;
-}
-
-
-Customer.prototype.getCustomer = function (shops) {
-    var max = 10000, short = -1;
-    for( var k = 0; k < shops.length; k++ ) {
-        if( Math.abs(shops[k].bumpTime - this.bumpTime) > span ) continue;
-        var dist = Math.sqrt(Math.pow(shops[k].latitude - this.latitude, 2)
-                             + Math.pow(shops[k].longtitude - this.longtitude, 2));
-        if( dist < max ) {
-            max = dist;
-            short = k;
-        }
-    }
-    return shops[short];
+QueuePool.prototype.add = function (queue) {
+    this.pool.push(queue);
 }
---
+++ server/thunder/model/queue.js
@@ -0,0 +1,16 @@
+module.export = Queue;
+
+function Queue(id, lat, lon, bt, time, type, serial, handle) {
+    this.id = id; //唯一的ID
+    this.latitude = lat; //经度
+    this.longtitude = lon; //纬度
+    this.bumpTime = bt; //碰撞时间
+    this.time = time; //接收时间
+    this.type = type; //碰撞时间
+    this.serial = serial; //团购券 Array()
+    this.socketHandle = handle; //SocketIoHandle
+
+    this.findResult = null; //[id]:other id, "error":find error
+    this.confirmStatus = null; //"yes", "no"
+    this.confirmResult = null; //"ok"
+}
